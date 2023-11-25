48808029d1c8a8a2dc49976df891023a48bb76df
Alix <alix@github.com>
2023-11-25 04:24:55 +0100

    Fix: Grammar with word 'broccoli'
    Takes 2 c not just one

--- items_menu.gd
+++ items_menu.gd
@@ -3,7 +3,7 @@ class_name ItemsMenu
 
 
 @onready var apples_spin_box: SpinBox = $ApplesMenu/SpinBox
-@onready var brocolis_spin_box: SpinBox = $BrocolisMenu/SpinBox
+@onready var broccolis_spin_box: SpinBox = $BroccolisMenu/SpinBox
 @onready var items_result_label: Label = $ItemsResultMenu/Label
 
 
@@ -22,22 +22,22 @@ func _on_apples_spin_box_value_changed(_value: float) -> void:
 	update_items_results_label()
 
 
-func _on_brocolis_spin_box_value_changed(_value: float) -> void:
+func _on_broccolis_spin_box_value_changed(_value: float) -> void:
 	update_items_results_label()
 
 
 func update_items_results_label() -> void:
 	var apples_amount: int = int(apples_spin_box.value)
-	var brocolis_amount: int = int(brocolis_spin_box.value)
+	var broccolis_amount: int = int(broccolis_spin_box.value)
 	
 	var apples_result: String = ""
-	var brocolis_result: String = ""
+	var broccolis_result: String = ""
 	
-	if apples_amount == 0 and brocolis_amount == 0:
+	if apples_amount == 0 and broccolis_amount == 0:
 		items_result_label.text = tr("You currently have no fruits!")
 		return
 	
 	apples_result = tr_n("You currently have %d apples!", "You currently have %d apples!", apples_amount) % apples_amount
-	brocolis_result = tr_n("You currently have %d brocolis!", "You currently have %d brocolis!", brocolis_amount) % brocolis_amount
+	broccolis_result = tr_n("You currently have %d broccolis!", "You currently have %d broccolis!", broccolis_amount) % broccolis_amount
 	
-	items_result_label.text = apples_result + "\n" + brocolis_result
+	items_result_label.text = apples_result + "\n" + broccolis_result
